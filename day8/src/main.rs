1c1e130c23e16ddbf8dc95771c501a9afc6a0dc1
Mark Sutherland <mark.sutherland@epfl.ch>
2023-12-26 09:21:42 +0100

    [WIP] Day8 part 2, still doesn't work after running for 10 mins

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -6,6 +6,7 @@ use regex::Regex;
 struct Network {
     directions: Vec<char>,
     nodes: HashMap<String, (String, String)>,
+    start_list: Vec<String>
 }
 
 impl Network {
@@ -41,6 +42,8 @@ impl NetworkBuilder {
             .split("\n")
             .collect();
 
+
+        let mut starting_node_strings: Vec<String> = Vec::new();
         // the first line contains all the directions (e.g., LRLR) so split that into
         // the Network's "directions" field
         net.directions = lines
@@ -56,14 +59,20 @@ impl NetworkBuilder {
         //  (\W+) = (\W+, \W+)
         let mut map: HashMap<String, (String, String)> = HashMap::new();
         //let re = Regex::new(r"([[:alpha]]+) = \(([[:alpha:]]), ([[:alpha:]])\)").unwrap();
-        let re = Regex::new(r"([[:alpha:]]+)\s=\s\x28([[:alpha:]]+),\s([[:alpha:]]+)\x29").unwrap();
+        let re = Regex::new(r"([[[:alpha:]]\d]+)\s=\s\x28([[[:alpha:]]\d]+),\s([[[:alpha:]]\d]+)\x29").unwrap();
         lines.iter()
             .for_each(|line| {
                 for (_, [node, l_str, r_str]) in re.captures_iter(line).map(|c| c.extract()) {
                     // We know all the keys are unique (tested and implied in the problem statement)
                     map.insert(node.to_string(), (l_str.to_string(), r_str.to_string()));
+
+                    // Put starting nodes on the list
+                    if node.ends_with('A') {
+                        starting_node_strings.push(node.to_string());
+                    }
                 }
             });
+        net.start_list = starting_node_strings;
         net.nodes = map;
         net
     }
@@ -84,25 +93,77 @@ fn main() {
     let fname = get_file_name();
     let network: Network = Network::builder(fname).build();
 
-    //let mut next_nodes: Vec<&String> = Vec::new();
-    let mut cur_node = "AAA".to_string();
-    let mut found = false;
-    let mut steps = 1;
-    while found == false {
-        // push all the nodes from the directions list onto the next nodes list
-        for idx in 0..network.directions.len() {
-            let next_node = match network.directions[idx] {
-                'L' => network.get_left_node(&cur_node),
-                'R' => network.get_right_node(&cur_node),
-                _ => unreachable!()
-            };
-            if next_node == "ZZZ" {
-                println!("Escaped in {} steps!", steps);
-                found = true;
-                break;
-            }
-            steps += 1;
-            cur_node = next_node;
-        }
+    // Part 1 - start from one node and traverse to the end 'ZZZ'
+    // let mut cur_node = "AAA".to_string();
+    // let mut found = false;
+    // let mut steps = 1;
+    // while found == false {
+    //     // push all the nodes from the directions list onto the next nodes list
+    //     for idx in 0..network.directions.len() {
+    //         let next_node = match network.directions[idx] {
+    //             'L' => network.get_left_node(&cur_node),
+    //             'R' => network.get_right_node(&cur_node),
+    //             _ => unreachable!()
+    //         };
+    //         if next_node == "ZZZ" {
+    //             println!("Escaped in {} steps!", steps);
+    //             found = true;
+    //             break;
+    //         }
+    //         steps += 1;
+    //         cur_node = next_node;
+    //     }
+    // }
+
+    fn get_next_node(direction_idx: usize, cur_node: &String, network: &Network) -> String {
+        let next_node = match network.directions[direction_idx] {
+            'L' => network.get_left_node(&cur_node),
+            'R' => network.get_right_node(&cur_node),
+            _ => unreachable!()
+        };
+        next_node
     }
+
+    // Part 2 - Start from all nodes ending in A, and go to all those ending in Z.
+    // Algorithm:
+    // - create two sets: one with the input nodes, one with the output
+    // - map each input node to an output
+    // - check for termination
+    // - swap the two set references, so the output becomes the input of the next step
+    let mut found = false;
+    let mut steps = 0;
+    let mut direction_idx: usize = 0;
+    // could optimize this to not clone the start list and strings all the time
+    let mut input_nodes = network.start_list.clone();
+    let mut output_nodes: Vec<String> = Vec::new();
+    let mut ptr_1 = &mut input_nodes;
+    let mut ptr_2 = &mut output_nodes;
+    while found == false {
+        //println!("Step {} starting with nodes {:?}...", steps, ptr_1);
+        ptr_1.into_iter().for_each(|node| {
+            ptr_2.push(get_next_node(direction_idx, &node, &network));
+        });
+        //println!("Transformed into nodes {:?}...", ptr_2);
+
+        // check termination
+        found = ptr_2.iter().all(|node| {
+            if node.ends_with('Z') {
+                //println!("Found that at least 1 node ends with Z! Step {}. Nodes = {:?}", steps, ptr_2);
+                return true;
+            } else {
+                return false;
+            }
+        });
+        ptr_1.clear();
+        // swap references and reset direction_idx for the next iteration
+        let tmp = ptr_1;
+        ptr_1 = ptr_2;
+        ptr_2 = tmp;
+        direction_idx += 1;
+        if direction_idx >= network.directions.len() {
+            direction_idx = 0;
+        }
+        steps += 1;
+    }
+    println!("Escaped in {} steps!", steps);
 }
