c760a036ec7da5a666810b3a42f8aeb0d850fe8e
Mark Sutherland <mark.sutherland@epfl.ch>
2025-03-04 08:17:06 +0100

    WIP for day9 part 1. Test input functional, real input too high.

--- 2024/cpp_proj/src/day9.cpp
+++ 2024/cpp_proj/src/day9.cpp
@@ -1,8 +1,18 @@
 #include <fstream>
 #include <iostream>
-#include <string>
+#include <vector>
+#include <xtensor/xadapt.hpp>
+#include <xtensor/xarray.hpp>
+#include <xtensor/xbuilder.hpp>
+#include <xtensor/xindex_view.hpp>
+#include <xtensor/xio.hpp>
+#include <xtensor/xmath.hpp>
+#include <xtensor/xtensor_forward.hpp>
+#include <xtensor/xutils.hpp>
+#include <xtensor/xview.hpp>
 
 const char ZERO = '0';
+const char FREE_CHAR = '.';
 
 char to_char(unsigned num) { return num + ZERO; }
 unsigned to_num(char c) { return c - ZERO; }
@@ -16,16 +26,92 @@ class DiskMap {
    * blocks.
    */
 private:
-  std::vector<unsigned> raw_disk;
+  std::vector<char> raw_disk;
+  xt::xarray<char> vec_disk;
 
 public:
   DiskMap(std::ifstream &file) {
     char c;
+    unsigned file_id = 0;
+    bool is_file = true;
     while (file >> c) {
-      raw_disk.push_back(to_num(c));
+      while (to_num(c--) > 0) {
+        if (is_file) {
+          raw_disk.push_back(to_char(file_id));
+        } else {
+          raw_disk.push_back('.');
+        }
+      }
+      if (is_file) {
+        file_id++;
+      }
+      is_file = !is_file;
     }
+
+    // Adapt the utility parsed disk var into the vectorized format.
+    vec_disk = xt::adapt(raw_disk);
   }
   friend std::ostream &operator<<(std::ostream &os, const DiskMap &disk);
+
+  void compact() {
+    // Compact the disk by doing the following:
+    // - build another vector whose values represent the INDEX of the final
+    // block that should
+    //   be placed at this location in the compacted disk.
+    // e.g., for a basic disk 0...1.222, we would generate
+    //                        087645123
+    //       - note that the "8" represents the last "2" coming into the 1st
+    //       position
+    //       - and so on and so forth with the 7 6.
+    //       - The 4 means that the 1 does not change its position
+    //       - The 5 means that the empty block won't be filled.
+    // - Then, the final compacted disk is simply going to be: final[i] =
+    // initial[index_swaps[i]]
+    //   which should be trivially vectorizable.
+    xt::xarray<size_t> index_swaps = xt::arange<size_t>(0, vec_disk.size(), 1);
+    size_t free_idx = 0, file_idx = vec_disk.size() - 1;
+    while (free_idx < file_idx) {
+      // std::cout << *this << "free_idx: " << free_idx
+      //           << ", file_idx: " << file_idx << std::endl;
+      if (vec_disk[free_idx] != FREE_CHAR) {
+        // std::cout << "Found file block: " << vec_disk[free_idx] << std::endl;
+        index_swaps[free_idx++] = free_idx;
+      } else {
+        // std::cout << "Found free block to compact at index " << free_idx
+        //           << std::endl;
+        while (vec_disk[file_idx] == FREE_CHAR) {
+          // std::cout << "Advancing file_block pointer looking for a file block
+          // "
+          //              "to compact... file_idx = "
+          // << file_idx << std::endl;
+          index_swaps[file_idx--] = file_idx;
+        }
+        // std::cout << "Swapping indices " << free_idx << " and " << file_idx
+        //           << std::endl;
+        // Getting here means that file_idx points to a non-free space block
+        // that we can swap into the place pointed to by 'free_idx'.
+        index_swaps[free_idx] = file_idx;
+        index_swaps[file_idx] = free_idx;
+        free_idx++;
+        file_idx--;
+        // std::cout << "Index swaps xarray: " << index_swaps << std::endl;
+      }
+    }
+    auto compacted = xt::index_view(vec_disk, index_swaps);
+    // std::cout << "Final compacted view: " << compacted << std::endl;
+    // Calculate the disk checksum.
+    // xt::xarray<size_t> indices = xt::arange<size_t>(0, vec_disk.size(), 1);
+    // auto product = (compacted - ZERO);
+    // std::cout << "Final product: " << product << std::endl;
+    unsigned long idx = 0;
+    unsigned long long checksum = 0;
+    for (auto &c : compacted) {
+      if (c != FREE_CHAR) {
+        checksum += (to_num(c) * idx++);
+      }
+    }
+    std::cout << "Final checksum: " << checksum << std::endl;
+  }
 };
 
 std::ostream &operator<<(std::ostream &os, const DiskMap &disk) {
@@ -33,13 +119,8 @@ std::ostream &operator<<(std::ostream &os, const DiskMap &disk) {
   // into its ID (repeated the number of blocks times), and free space (repeated
   // as dots)
   os << "Disk Map: ";
-  unsigned file_id = 0;
-  for (size_t i = 0; i < disk.raw_disk.size(); i++) {
-    if (i & (unsigned)0x1) { // Odd index, represents free space.
-      os << std::string(disk.raw_disk[i], '.');
-    } else { // Even index, represents file space.
-      os << std::string(disk.raw_disk[i], to_char(file_id++));
-    }
+  for (auto &c : disk.vec_disk) {
+    os << c;
   }
   os << std::endl;
   return os;
@@ -58,6 +139,6 @@ int main(int argc, char *argv[]) {
   }
 
   DiskMap disk(file);
-  std::cout << disk << std::endl;
+  disk.compact();
   return 0;
 }
